da65b8c628b64f5640fa8c9994116f8c959ecf9d
Ausrine <ausrine.venslaviciute@stud.vgtu.lt>
2023-01-24 23:00:56 +0200

    FrontEnd'as

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UiCardComponent } from './ui-card/ui-card.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UiCardComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/ui-card/ui-card.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {FormGroup, FormControl, Validators, FormBuilder, NgForm} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from "rxjs";
+
+@Component({
+  selector: 'app-ui-card',
+  templateUrl: './ui-card.component.html',
+  styleUrls: ['./ui-card.component.scss']
+})
+export class UiCardComponent {
+
+  age: number;
+  isStudent: boolean;
+  income: number;
+  info: string[];
+
+  constructor(private http: HttpClient) { }
+
+  onSubmit() {
+    const answers = {
+      age: this.age,
+      isStudent: this.isStudent,
+      income: this.income
+    };
+    this.http.post<string[]>
+    ('http://localhost:8080/info', answers)
+      .subscribe(response => this.info = response);
+  }
+
+}
